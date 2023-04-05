ef1fee2c739d87d0593b5bb819142c3cc96efc94
ahmadhabibi14 <ahmadhabibi7159@gmail.com>
2023-04-06 02:37:55 +0800

    Routing navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,9 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind({
+    // Example: Disable injecting a basic `base.css` import on every page.
+    // Useful if you need to define and/or import your own custom `base.css`.
+    config: { applyBaseStyles: false },
+  })],
 });
