360d19c81433b47f794670c0d060b1aa7f2050d4
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2024-05-19 13:43:02 +0800

    update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,30 +1,17 @@
-import "./globals.css"
-import { Suspense } from "react"
-import Navbar from "@/components/partials/navbar"
-import Footer from "@/components/partials/footer"
-import ErrorBoundary from "@/components/partials/error-boundary"
-import Error from "./error"
+import "./globals.css";
 
 export const metadata = {
-   icons: {
-      icon: "/favicons/favicon.svg"
-   }
- }
+  icons: {
+    icon: "/favicons/favicon.svg",
+  },
+};
 
 export default function RootLayout({ children }) {
-   return (
-      <html lang="en">
-         <body className="font-poppins bg-gray-50 text-gray-900">
-            <Navbar />
-            <div className="min-h-[85vh]">
-               <ErrorBoundary fallback={<Error />}>
-                  <Suspense fallback={<p>Loading....</p>}>
-                     {children}
-                  </Suspense>
-               </ErrorBoundary>
-            </div>
-            <Footer />
-         </body>
-      </html>
-   )
+  return (
+    <html lang="en">
+      <body className="font-poppins bg-gray-950 text-gray-100">
+        {children}
+      </body>
+    </html>
+  );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,36 +1,29 @@
-import Heading from "@/components/pages/home/heading";
-import Section01 from "@/components/pages/home/section_01";
-import Section02 from "@/components/pages/home/section_02";
-import Section03 from "@/components/pages/home/section_03";
 const currentTime = new Date().toISOString();
 
 export const metadata = {
-   title: "Yome - Listen your fantastic audio",
-   description: "A Portal to get your favorite music",
-   generator: "Next.js",
-   applicationName: "Yome",
-   referrer: "origin-when-cross-origin",
-   keywords: ["Spotify", "Music"],
-   authors: [{ name: "Ahmad Habibi", url: "https://habibi2004.pages.dev" }],
-   creator: "Yome.Inc",
-   publisher: "Yome.Inc",
-   icons: {
-      icon: "/favicons/favicon.svg",
-   },
-   openGraph: {
-      title: "Yome - Listen your fantastic audio",
-      images: "/img/cover/default.png",
-      publishedTime: currentTime,
-   },
-}
+  title: "Yome - Listen your fantastic audio",
+  description: "A Portal to get your favorite music",
+  generator: "Next.js",
+  applicationName: "Yome",
+  referrer: "origin-when-cross-origin",
+  keywords: ["Spotify", "Music"],
+  authors: [{ name: "Ahmad Habibi", url: "https://habibi2004.pages.dev" }],
+  creator: "Ahmad Habibi",
+  publisher: "Ahmad Habibi",
+  icons: {
+    icon: "/favicons/favicon.svg",
+  },
+  openGraph: {
+    title: "Yome",
+    images: "/img/cover/default.png",
+    publishedTime: currentTime,
+  },
+};
 
 export default function Home() {
-   return (
-      <main className="w-full mt-20">
-         <Heading />
-         <Section01 />
-         <Section03 />
-         <Section02 />
-      </main>
-   )
+  return (
+    <main className="w-full mt-20">
+      <h2>Hello</h2>
+    </main>
+  );
 }
